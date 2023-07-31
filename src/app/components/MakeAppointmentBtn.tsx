7293d6c678e27ca427ea22f4074be339eb6af8d5
Janith Madarasinghe <janithpm9991@gmail.com>
2023-07-31 15:23:14 +0530

    Update MakeAppointmentBtn.tsx

--- src/app/components/MakeAppointmentBtn.tsx
+++ src/app/components/MakeAppointmentBtn.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Link from 'next/link'
 import { useState, useEffect } from 'react'
 
 function MakeAppointmentBtn() {
@@ -18,14 +19,14 @@ function MakeAppointmentBtn() {
     }, [])
 
     return (
-        <div
+        <Link href="/services"
             className={`${!show && 'hidden'}
             bg-red-700 text-white text-center px-6 py-[8px] rounded  hover:bg-red-600 transition duration-300 ease-in-out cursor-pointer
             fixed right-1/2 transform translate-x-1/2 md:right-10 md:translate-x-0 bottom-10
             hover:scale-105
             floating-btn
             `}
-        >Make An Appointment</div>
+        >Make An Appointment</Link>
     )
 }
 
