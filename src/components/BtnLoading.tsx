190ac36603cddb0a94c5794ecb2ac97032cdd53b
Janith Madarasinghe <73998693+Janithpm@users.noreply.github.com>
2023-11-01 11:42:27 +0530

    btn loading scsss

--- src/components/BtnLoading.tsx
+++ src/components/BtnLoading.tsx
@@ -1,5 +1,5 @@
 
-import '@/styles/BtnLoading.scss'
+import '../styles/BtnLoading.scss'
 function BtnLoading() {
     return (
         <div className=" py-[2px] flex justify-center items-center w-full h-full"><span className="btnloader"></span></div>
